4d877ed79e1833736ba82ae4527bf5be909fe2fd
Raghammudi Veda Vamsi Krishna <cb.sc.u4cse25346@cb.students.amrita.edu>
2026-02-04 21:28:30 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,35 @@
+// 9. Calculator using Switch-Case
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        System.out.println("Choose operation: +, -, *, /");
+        char operation = sc.next().charAt(0);
+
+        switch (operation) {
+            case '+':
+                System.out.println("Result: " + (num1 + num2));
+                break;
+            case '-':
+                System.out.println("Result: " + (num1 - num2));
+                break;
+            case '*':
+                System.out.println("Result: " + (num1 * num2));
+                break;
+            case '/':
+                if (num2 != 0) {
+                    System.out.println("Result: " + (num1 / num2));
+                } else {
+                    System.out.println("Division by zero error");
+                }
+                break;
+            default:
+                System.out.println("Invalid operation");
+        }
+    }
+}
